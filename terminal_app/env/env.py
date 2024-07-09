80dc432154ae09a3e411f8e2937c1cdde9f17f65
Antonio <antonio.projects.studio@gmail.com>
2024-07-09 20:53:40 +0300

    source prod, one config folder

--- terminal_app/env/env.py
+++ terminal_app/env/env.py
@@ -3,7 +3,6 @@ import os
 import sys
 import platform
 
-from typing import Any
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -11,51 +10,52 @@ OS = platform.system().lower()
 
 BASE_DIR = Path(__main__.__file__).parent.parent
 CONFIG_DIR = BASE_DIR / "configs"
-DEV_OS_DIR = CONFIG_DIR / f"development_{OS}"
+DEV_DIR = CONFIG_DIR / f"development"
+PROD_DIR = CONFIG_DIR / f"production"
 
 PYTHON_DIR = Path(__main__.__file__).parent
 
-MODE = os.getenv("MODE") or "development"
+MODE = os.getenv("PROD") or "development"
+CURRENT_DIR = DEV_DIR if MODE == "development" else PROD_DIR
 
 if not CONFIG_DIR.exists():
     os.mkdir(CONFIG_DIR)
 
-if not DEV_OS_DIR.exists():
-    os.mkdir(DEV_OS_DIR)
+if not DEV_DIR.exists():
+    os.mkdir(DEV_DIR)
 
 
 def source(env_files: str | list[str]) -> dict[str, str]:
-    #TODO ignore " in .env
+    # TODO ignore " in .env
     data: dict[str, str] = {}
 
     def _source(env_files: str) -> None:
-        file_path = DEV_OS_DIR / env_files
+        file_path = CURRENT_DIR / env_files
         if not file_path.exists():
             with open(file_path, "w"):
                 pass
             print(f"Create {file_path}")
         else:
-            load_dotenv(DEV_OS_DIR / env_files)
+            load_dotenv(CURRENT_DIR / env_files)
 
     def load_variables(env_files: str) -> None:
-        file_path = DEV_OS_DIR / env_files
+        file_path = CURRENT_DIR / env_files
         with open(file_path) as f:
             for line in f.readlines():
                 line = line.strip()
                 if not line.startswith("#"):
-                    name = line[: line.find("=")].strip()
-                    arg = line[line.find("=") + 1 :].strip()
+                    name = line[: line.find("=")].strip().strip('"').strip("'")
+                    arg = line[line.find("=") + 1 :].strip().strip('"').strip("'")
                     data[name] = arg
 
-    if MODE == "development":
-        if isinstance(env_files, str):
-            _source(env_files)
-            load_variables(env_files)
+    if isinstance(env_files, str):
+        _source(env_files)
+        load_variables(env_files)
 
-        else:
-            for path in env_files:
-                _source(path)
-                load_variables(path)
+    else:
+        for path in env_files:
+            _source(path)
+            load_variables(path)
 
     return data
 
