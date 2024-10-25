a83771a20d607e901fbeaa21f72a83e4b2461e1a
Antonio Rodrigues <antonio.projects.studio@gmail.com>
2024-10-25 18:28:10 +0300

    bug fix in certify, add json serialize

--- terminal_app/curlify/curlify.py
+++ terminal_app/curlify/curlify.py
@@ -1,16 +1,24 @@
+import json
 import requests
 import flask
 import urllib.parse
 
+
 class Curlify:
-    def __init__(self, request: requests.PreparedRequest | requests.Request | flask.Request, localhost=False, compressed=False, verify=True):
+    def __init__(
+        self,
+        request: requests.PreparedRequest | requests.Request | flask.Request,
+        localhost=False,
+        compressed=False,
+        verify=True,
+    ):
         self.request = request
         self.url = self.request.url
 
         if localhost:
             parsed = urllib.parse.urlparse(self.request.url)
             parsed = parsed._replace(netloc=f"127.0.0.1:{parsed.port}")
-            self.url =urllib.parse.urlunparse(parsed)
+            self.url = urllib.parse.urlunparse(parsed)
         self.compressed = compressed
         self.verify = verify
 
@@ -20,19 +28,22 @@ class Curlify:
         Returns:
             str: return string of set headers
         """
+
         def validation(k, v):
             if k == "Host":
                 v = self.url
             return f'"{k}: {v}"'
-        
+
         headers = [validation(k, v) for k, v in self.request.headers.items()]
 
         return " -H ".join(headers)
 
     def body(self) -> str | None:
         if not isinstance(self.request, requests.PreparedRequest):
+            if isinstance(self.request.data, dict):
+                return json.dumps(self.request.data)
             return str(self.request.data, "utf-8")
-        
+
         return (
             self.request.body
             if isinstance(self.request.body, str | None)
